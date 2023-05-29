f0ae729ce0e8c365c20872852ee0272f5cf7c422
SuppliedOrange <70258998+SuppliedOrange@users.noreply.github.com>
2023-05-29 00:13:05 -0700

    v1.1

---
+++ js/fixcanvas.js
@@ -0,0 +1,15 @@
+addEventListener("load", async (event) => {
+
+    const thumbnail = document.getElementsByTagName('img')[0]
+    if (!thumbnail) return;
+
+    let selfie = localStorage.getItem("selfie");
+    if (!selfie) return;
+
+    selfie = await JSON.parse( selfie );
+
+    let lesser = ( selfie.height > selfie.width ) ? selfie.width : selfie.height;
+
+    thumbnail.setAttribute('style', `transform: scaleX(1); width: ${lesser}px; height: ${lesser}px`);
+
+});
